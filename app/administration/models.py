c85e6b972590894983590c564a7009b78b227098
Rojas-Andres <andresfelipe200004@gmail.com>
2022-07-08 11:55:49 -0500

    implementacion crear cliente

--- app/administration/models.py
+++ app/administration/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Client(models.Model):
     name_client = models.CharField(verbose_name="Nombre", max_length=255)
-    nit = models.CharField(verbose_name="Nit", max_length=255, unique=True)
+    nit = models.CharField(verbose_name="Nit", max_length=9, unique=True)
 
     class Meta:
         verbose_name = "Client"
--- app/administration/views.py
+++ app/administration/views.py
@@ -2,7 +2,7 @@ from django.template import RequestContext
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 from rest_framework.decorators import api_view
-
+from administration.models import Client
 from django.contrib.auth.decorators import login_required
 
 
@@ -19,4 +19,29 @@ def clients(request):
 @login_required
 @api_view(["GET", "POST"])
 def create_client(request):
-    return render(request, "clients/create_client.html")
+    context = {}
+    if request.method == "GET":
+        return render(request, "clients/create_client.html")
+
+    nit = request.POST["nit"]
+    nombre = request.POST["nameClient"]
+    client = Client.objects.filter(nit=nit)
+    if client:
+        context[
+            "msj"
+        ] = "Ya se encuentra un cliente con ese NIT, por lo tanto no se crea"
+        context["alert"] = "danger"
+        return render(request, "clients/create_client.html", {"context": context})
+
+    if len(nit) > 9:
+        context["msj"] = "El nit no puede tener mas de 9 digitos"
+        context["alert"] = "danger"
+        return render(request, "clients/create_client.html", {"context": context})
+
+    new_client = Client.objects.create(name_client=nombre, nit=nit)
+    new_client.save()
+
+    context["msj"] = "Cliente creado correctamente"
+    context["alert"] = "success"
+
+    return render(request, "clients/create_client.html", {"context": context})
