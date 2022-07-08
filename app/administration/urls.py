349084df7e2f980b7ecca86cebbd62cdf631c981
Rojas-Andres <andresfelipe200004@gmail.com>
2022-07-08 15:04:41 -0500

    implementacion actualizar usuario

--- app/administration/urls.py
+++ app/administration/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 from django.urls import path
 from administration import views
+from django.conf.urls import url
 
 urlpatterns = [
     path("client/", views.clients, name="clients"),
     path("create_client/", views.create_client, name="create_client"),
+    path("show_clients/", views.show_clients, name="show_clients"),
+    path("update_client/", views.update_client, name="update_client"),
+    path("update_client/<int:id_client>", views.update_client, name="update_client"),
 ]
--- app/administration/views.py
+++ app/administration/views.py
@@ -1,5 +1,5 @@
 from django.template import RequestContext
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from rest_framework.decorators import api_view
 from administration.models import Client
@@ -14,6 +14,14 @@ def Index(request):
 @api_view(["GET"])
 def clients(request):
     return render(request, "clients/clients.html")
+
+
+@login_required
+@api_view(["GET"])
+def show_clients(request):
+    clients = Client.objects.all()
+    clients = [{"nombre": i.name_client, "nit": i.nit, "id": i.id} for i in clients]
+    return render(request, "clients/show_clients.html", {"data": clients})
 
 
 @login_required
@@ -45,3 +53,26 @@ def create_client(request):
     context["alert"] = "success"
 
     return render(request, "clients/create_client.html", {"context": context})
+
+
+@login_required
+@api_view(["GET", "POST"])
+def update_client(request, id_client=None):
+    client = Client.objects.filter(id=id_client).first()
+    data = {"nameClient": client.name_client, "nit": client.nit, "id": client.id}
+    if request.method == "GET":
+        return render(
+            request,
+            "clients/update_client.html",
+            data,
+        )
+    nit = request.POST["nit"]
+    if len(nit) > 9:
+        data["msj"] = "El nit no puede tener mas de 9 digitos"
+        data["alert"] = "danger"
+        return render(request, "clients/update_client.html", data)
+    client.nit = nit
+    client.name_client = request.POST["nameClient"]
+    client.save()
+
+    return redirect("show_clients")
